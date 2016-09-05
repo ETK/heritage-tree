ef582252da221bf38e16c66082ccaecc8d5e5ee7
jszymanowski <j.szymanowski@gmail.com>
2016-09-04 20:12:48 -0400

    Minor changes to name formatting

--- db/models/person.js
+++ db/models/person.js
@@ -26,6 +26,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.VIRTUAL,
       get: function() {
         var name = this.last_name + ', ' + this.first_name;
+        if(this.nick_name) name += ' "' + this.nick_name + '"';
         if(this.middle_name) name += ' ' + this.middle_name;
         if(this.suffix) name += ' ' + this.suffix;
         return name;
